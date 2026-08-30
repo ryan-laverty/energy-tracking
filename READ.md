8a297c32fa836da272ae8328107cafd52ba73a36
ryanlaverty <ryanlaverty03@gmail.com>
2026-08-30 19:27:13 +0100

    Initial project commit: Added User Service & accessor methods to add, update, fetch, and delete a user from DB instance.

---
+++ READ.md
@@ -0,0 +1,16 @@
+# Getting Started
+
+## How to start
+First thing you need to do is run the docker containers.
+
+run the following command in the root directory of the project:
+```
+docker compose -v up -d
+```
+
+
+to stop the docker containers do
+```
+docker compose down
+```
+
---
+++ user-service/src/main/java/com/leetjourney/user_service/controller/UserController.java
@@ -0,0 +1,55 @@
+package com.leetjourney.user_service.controller;
+
+import com.leetjourney.user_service.dto.UserDto;
+import com.leetjourney.user_service.service.UserService;
+import org.apache.catalina.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController (UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+        UserDto created = userService.createUser(userDto);
+        return new ResponseEntity<>(created, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
+        UserDto userDto = userService.getUserById(id);
+        if (userDto == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(userDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable Long id,
+                                             @RequestBody UserDto userDto) {
+        try {
+            userService.updateUser(id, userDto);
+            return ResponseEntity.ok("User updated successfully");
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>("user not found", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        try {
+            userService.deleteUser(id);
+            return ResponseEntity.noContent().build();
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
