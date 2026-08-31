89c7930e242d7955d9e249c5177e455f1e4a1812
ryanlaverty <ryanlaverty03@gmail.com>
2026-08-31 21:06:17 +0100

    Added device service with ability to add/edit/get/delete device from DB

---
+++ device-service/src/main/java/com/leetjourney/device_service/repository/DeviceRepository.java
@@ -0,0 +1,9 @@
+package com.leetjourney.device_service.repository;
+
+import com.leetjourney.device_service.entity.Device;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DeviceRepository extends JpaRepository<Device, Long> {
+}
