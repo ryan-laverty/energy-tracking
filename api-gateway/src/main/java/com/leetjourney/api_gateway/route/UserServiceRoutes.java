91ebec946f4acd44b518c776d398c5548cb89ed0
ryanlaverty <ryanlaverty03@gmail.com>
2026-09-15 19:43:29 +0100

    Added OpenAPI docs for device / user-service / api-gateway

--- api-gateway/src/main/java/com/leetjourney/api_gateway/route/UserServiceRoutes.java
+++ api-gateway/src/main/java/com/leetjourney/api_gateway/route/UserServiceRoutes.java
@@ -1,6 +1,7 @@
 package com.leetjourney.api_gateway.route;
 
 import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.function.ServerResponse;
 import java.net.URI;
 
 import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 
@@ -35,6 +37,16 @@ public class UserServiceRoutes {
                 .route(RequestPredicates.path("/fallbackRoute"),
                         request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
                                 .body("User service is down"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> userServiceApiDocs() {
+        return GatewayRouterFunctions.route("user-service-api-docs")
+                .route(RequestPredicates.path("/docs/user-service/v3/api-docs"),
+                        http())
+                .before(uri("http://localhost:8080"))
+                .filter(setPath("/v3/api-docs"))
                 .build();
     }
 }
---
+++ user-service/src/main/java/com/leetjourney/user_service/config/OpenApiConfig.java
@@ -0,0 +1,36 @@
+package com.leetjourney.user_service.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI userServiceApiDocs() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("User Service API")
+                        .description("User service API for Home Energy Tracker")
+                        .contact(getContact())
+                        .license(getLicense())
+                        .version("1.0.0"));
+    }
+
+    private static License getLicense() {
+        License license = new License();
+        license.setName("Creative Commons Attribution-NonCommerical 4.0 International License");
+        license.setUrl("https://creativecommons.org/licenses/by-nc/4.0/");
+        return license;
+    }
+
+    private static Contact getContact() {
+        Contact contact = new Contact();
+        contact.setUrl("https://leetjourney.com");
+        contact.setEmail("ryanlaverty03@gmail.com");
+        return contact;
+    }
+}
